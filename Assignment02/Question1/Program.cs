60e2b94c8a03cd626802c9f782f7c42f5b2a823e
shreyashg-19 <shreyashghare0019@gmail.com>
2024-12-28 11:22:23 +0530

    Assignment02 Uploaded

---
+++ Assignment02/Question1/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.acceptStudent();
+            s.displayStudents();
+        }
+        public struct Student
+        {
+            public string name;
+            public bool gender;
+            public int age;
+            public int std;
+            public char div;
+            public double marks;
+
+            public Student()
+            {
+                string name = "";
+                bool gender = true;
+                int age = 0;
+                int std = 0;
+                char div = 'A';
+                double marks = 0;
+            }
+
+            public Student(string name, bool gender, int age, int std, char div, double marks)
+            {
+                this.name = name;
+                this.gender = gender;
+                this.age = age;
+                this.std = std;
+                this.div = div;
+                this.marks = marks;
+            }
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public bool Gender
+            {
+                get { return gender; }
+                set { gender = value; }
+            }
+
+            public int Age
+            {
+                get { return age; }
+                set { age = value; }
+            }
+
+            public int Std
+            {
+                get { return std; }
+                set { std = value; }
+            }
+
+            public char Div
+            {
+                get { return div; }
+                set { div = value; }
+            }
+
+            public double Marks
+            {
+                get { return marks; }
+                set { marks = value; }
+            }
+
+            public void acceptStudent()
+            {
+                Console.WriteLine("\nEnter Student Name - ");
+                name = Console.ReadLine();
+                Console.WriteLine("\nEnter Student Gender - ");
+                gender = bool.Parse(Console.ReadLine());
+                Console.WriteLine("\nEnter Student Age - ");
+                age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nEnter Student Standard");
+                std = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nEnter Students Division - ");
+                div = char.Parse(Console.ReadLine());
+                Console.WriteLine("\nEnter Students Marks - ");
+                marks = double.Parse(Console.ReadLine());
+            }
+
+            public void displayStudents()
+            {
+                Console.WriteLine("Name = " + name);
+                Console.WriteLine("Gender = " + gender);
+                Console.WriteLine("Age = " + age);
+                Console.WriteLine("Standard = " + std);
+                Console.WriteLine("Division = " + div);
+                Console.WriteLine("Marks = " + marks);
+            }
+        }
+    }
+}
---
+++ Assignment02/Question2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = CreateArray();
+            AcceptInfo(students);
+
+            Console.WriteLine("\nOriginal Student Information:");
+            PrintInfo(students);
+
+            Student[] reversedStudents = new Student[students.Length];
+            ReverseArray(students, reversedStudents);
+
+            Console.WriteLine("\nReversed Student Information:");
+            PrintInfo(reversedStudents);
+        }
+
+        public static Student[] CreateArray()
+        {
+            Console.Write("Enter the number of students: ");
+            int size = int.Parse(Console.ReadLine());
+
+            return new Student[size];
+        }
+
+        public static void AcceptInfo(Student[] students)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"\nEntering details for Student {i + 1}:");
+
+                Console.Write("Enter Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter Gender (true for Male, false for Female): ");
+                bool gender = bool.Parse(Console.ReadLine());
+
+                Console.Write("Enter Age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter Standard: ");
+                int std = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter Division: ");
+                char div = char.Parse(Console.ReadLine());
+
+                Console.Write("Enter Marks: ");
+                double marks = double.Parse(Console.ReadLine());
+
+                students[i] = new Student(name, gender, age, std, div, marks);
+            }
+        }
+
+        public static void PrintInfo(Student[] students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        public static void ReverseArray(Student[] original, Student[] reversed)
+        {
+            int n = original.Length;
+            for (int i = 0; i < n; i++)
+            {
+                reversed[i] = original[n - i - 1];
+            }
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public bool gender;
+        public int age;
+        public int std;
+        public char div;
+        public double marks;
+
+        public Student(string name, bool gender, int age, int std, char div, double marks)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+        }
+
+        public override string ToString()
+        {
+            string gend = gender ? "Male" : "Female";
+            return $"Name: {name}, Gender: {gend}, Age: {age}, Standard: {std}, Division: {div}, Marks: {marks}";
+        }
+    }
+}
